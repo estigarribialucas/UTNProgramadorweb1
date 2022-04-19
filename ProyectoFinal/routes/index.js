c51685060a85d43eef99343dd8f9347da42c77de
Lucas Estigarribia <estigarribia.l97@gmail.com>
2022-04-19 17:16:12 -0300

    Agregado de tienda y detalles

---
+++ ProyectoFinal/routes/index.js
@@ -0,0 +1,38 @@
+var express = require('express');
+var router = express.Router();
+var nodemailer = require('nodemailer');
+
+/* GET home page. */
+router.get('/', function (req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.post('/'), async (req, res, next) => {
+  var nombre = req.body.nombre;
+  var email = req.body.email;
+  var mensaje = req.body.mensaje;
+
+  console.log(req.body)
+
+  var obj = {
+    to: 'estigarribia.l97@gmail.com',
+    subject: 'CONTACTO WEB',
+    html: nombre + " se contacto a través de la web y quiere más informacion a este correo : " + email + ". <br> Además, hizo este comentario : " + mensaje + ", <br> Su tel es: " + tel
+  }
+
+  var transport = nodemailer.createTransport({
+    host: process.env.SMTP_HOST,
+    port: process.env.SMTP_PORT,
+    auth: {
+      user: process.env.SMTP_USER,
+      pass: process.env.SMTP_PASS
+    }
+  });
+  var info = await transport.sendMail(obj);
+
+  res.render('index', {
+    menssage: 'Mensaje enviado correctamente'
+  });
+};
+
+module.exports = router;
